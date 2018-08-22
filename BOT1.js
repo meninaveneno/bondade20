9154d176fb74293da26292e38ccedf36e6b7e791
meninaveneno <40373807+meninaveneno@users.noreply.github.com>
2018-08-22 10:57:18 -0300

    Update BOT1.js

--- BOT1.js
+++ BOT1.js
@@ -99,7 +99,7 @@
     var loadChat = function (cb) {
         if (!cb) cb = function () {
         };
-        $.get("https://rawgit.com/Shadow-Slayer/mib/master/pt-br.json", function (json) {
+        $.get("https://rawgit.com/meninaveneno/bondade20/master/PT-BR.json", function (json) {
             var link = basicBot.chatLink;
             if (json !== null && typeof json !== "undefined") {
                 langIndex = json;
@@ -240,9 +240,9 @@
         status: false,
         name: "Move It-Bot",
         loggedInID: null,
-        scriptLink: "https://rawgit.com/basicBot/mib/master/bot.js",
+        scriptLink: null,
         cmdLink: "http://git.io/245Ppg",
-        chatLink: "https://rawgit.com/Shadow-Slayer/mib/master/pt-br.json",
+        chatLink: "https://rawgit.com/meninaveneno/bondade20/master/PT-BR.json",
         chat: null,
         loadChat: loadChat,
         retrieveSettings: retrieveSettings,
@@ -250,8 +250,8 @@
         settings: {
             botName: "BOT-Seduzente",
             language: "portuguese",
-            chatLink: "https://rawgit.com/Shadow-Slayer/mib/master/pt-br.json",
-            scriptLink: "https://rawgit.com/basicBot/mib/master/bot.js",
+            chatLink: "https://rawgit.com/meninaveneno/bondade20/master/PT-BR.json",
+            scriptLink: null,
             roomLock: false, // Requires an extension to re-load the script
             startupCap: 1, // 1-200
             startupVolume: 0, // 0-100
